96862ae2366711d2b111e65b00238228ebb42663
Leo Sibayan <rafaellosibayan36@gmail.com>
2025-03-07 22:28:27 +0800

    Added required model classes to models.py

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    class Meta:
+        ordering = ['name']
+        
+    def __str__(self):
+        return self.name
+        
+
+class Post(models.Model):
+    name = models.CharField(max_length=255)
+    entry = models.TextField()
+    createdOn = models.DateTimeField()
+    updatedOn = models.DateTimeField()
+    category = models.ForeignKey(
+        PostCategory,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    
+    class Meta:
+        ordering = ['-createdOn']
+        
+    def __str__(self):
+        return self.name
+    
