c87d5b30baad5acdce51489fbf0e34b5d24dc251
tentbrella <maxneisidro@gmail.com>
2025-03-08 12:37:05 +0800

    Added url paths, modified templates
    Added url paths and set the format for the wiki_list (article category) template

---
+++ wiki/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ArticleCatergoryView, ArticleView
+
+app_name = "wiki"
+
+urlpatterns = [
+    path('articles/', ArticleCatergoryView.as_view(), name='article-list')
+    path('detail/<int:pk>', ArticleView.as_view(), name='article-detail')
+    ]
