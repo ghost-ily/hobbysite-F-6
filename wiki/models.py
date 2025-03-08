358ee9432d36ded6116e2f3b6caec99e9df78323
tentbrella <maxneisidro@gmail.com>
2025-03-08 08:11:59 +0800

    Added Models
    Added ArticleCategory and Article Model to models.py

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.CASCADE, related_name='category')
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    
