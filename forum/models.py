363cf320fdd2435103926cf3cae6458b32a3eedb
Leo <rafaellosibayan36@gmail.com>
2025-03-08 19:10:23 +0800

    Grouped posts on list view by PostCategory

--- forum/models.py
+++ forum/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
         ordering = ['-createdOn']
         
     def __str__(self):
-        return self.name
+        return self.title
         
     def get_absolute_url(self):
         return reverse('forum:post_detail', args=[self.pk])
