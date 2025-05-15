6cafa24c73060cfc8bcec445660b3181adfdb8a1
enzo1013 <enzopamintuan.1013@gmail.com>
2025-05-15 14:27:29 +0800

    Updated forms, views and urls.py; not yet working

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import index, ProductListView, ProductDetailView
+from .views import (
+    index, ProductListView, ProductDetailView, ProductCreateView,
+    ProductUpdateView
+)
 
 urlpatterns = [
     path('merchstore/', index, name='index'),
     path('merchstore/items/', ProductListView.as_view(), name='list'),
     path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='detail'),
+    path('merchstore/item/create', ProductCreateView.as_view(), name='create'),
+    path('merchstore/item/<int:pk>/edit', ProductUpdateView.as_view(), name='update'),
 ]
 
 app_name = 'merchstore'
--- merchstore/views.py
+++ merchstore/views.py
@@ -2,16 +2,46 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import ProductForm
 
 # Create your views here.
 class ProductListView(ListView):
     model = Product
     template_name = 'list.html'
 
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['product'] = Product.objects.all()
+        ctx['form'] = ProductForm()
+        return ctx
+    
+    def post(self, request, *args, **kwargs):
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return self.get(request, *args, **kwargs)
+        else:
+            self.object_list = self.get_queryset()
+            ctx = self.get_context_data(**kwargs)
+            ctx['form'] = form
+            return self.return_to_response(ctx)
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'create.html'
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'create.html'
 
 def index(request):
     return HttpResponse('Hello world! Go to /merchstore/items to begin.')
