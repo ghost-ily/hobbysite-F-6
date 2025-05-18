f5753137d90356bfcf50ca97116bb342dc17eb01
Leo Sibayan <rafaellosibayan36@gmail.com>
2025-05-19 02:35:08 +0800

    Edited PostDetailView view and template to specs.
    Added comment section and form.

--- forum/forms.py
+++ forum/forms.py
@@ -6,3 +6,12 @@ class ThreadForm(forms.ModelForm):
     class Meta:
         model = Thread
         fields = ['title', 'entry', 'category']
+        
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['entry']
+        labels = {
+            "entry": "Comment"
+        }
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,10 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.shortcuts import render
-from .models import ThreadCategory, Thread
-from .forms import ThreadForm
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from .models import ThreadCategory, Thread, Comment
+from .forms import ThreadForm, CommentForm
 
 
 def list_threads(request):
@@ -25,5 +26,19 @@ class CreateThreadView(CreateView):
 class PostDetailView(DetailView):
     model = Thread
     template_name = 'post_detail.html'
-
-# Create your views here.
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm()
+        return context
+        
+    def post(self, request, *args, **kwargs):
+        thread_pk = self.kwargs['pk']
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment()
+            comment.entry = form.cleaned_data.get('entry')
+            comment.thread = Thread.objects.get(pk=thread_pk)
+            comment.save()
+        success_url = reverse_lazy("forum:post_detail", kwargs={'pk':thread_pk})
+        return redirect(success_url)
