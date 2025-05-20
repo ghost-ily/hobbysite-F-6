62d200f6db5865587d8277fd8d0c3c8f1840663c
Leo Sibayan <rafaellosibayan36@gmail.com>
2025-05-20 13:19:20 +0800

    Delete redundant folders.

--- hobbysite/commissions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CommissionListView, CommissionDetailView
-
-app_name = 'commissions'
-
-urlpatterns = [
-    path('list/', CommissionListView.as_view(), name='commission-list'),
-    path('list/detail/<int:pk>/', CommissionDetailView.as_view(), name='commission-detail'),
-]
--- hobbysite/wiki/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ArticleCatergoryView, ArticleView
-
-app_name = "wiki"
-
-urlpatterns = [
-    path('articles/', ArticleCatergoryView.as_view(), name='category'),
-    path('detail/<int:pk>/', ArticleView.as_view(), name='article-detail')
-    ]
